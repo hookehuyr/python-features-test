0ced605c520762dc4170aa5151df0db59762dba2
hookehuyr <hookehuyr@gmail.com>
2025-04-27 17:12:57 +0800

    test(t-8): 添加1.0.py文件以测试datetime模块
    添加1.0.py文件用于测试Python的datetime模块功能，包含基本的导入和打印操作。

---
+++ src/test_demo/t-8/1.0.py
@@ -0,0 +1,9 @@
+'''
+Date: 2025-04-27 17:06:37
+LastEditors: hookehuyr hookehuyr@gmail.com
+LastEditTime: 2025-04-27 17:07:11
+FilePath: /test-demo/src/test_demo/t-8/1.0.py
+Description: 文件描述
+'''
+import datetime
+print(dir(datetime))
